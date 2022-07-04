b8a33ee8f967279a55c608876bf997b742d2c7be
Marcel Eichler <marcel.eichler@s-und-n.de>
2022-07-04 15:45:16 +0200

    changed project params

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var handleRequest = function(request, response) {
   response.end("<h1> Deployed Version is v1.0.</h1>");
 }
 var www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(80);
